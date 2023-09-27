ddea7feb7f9cd0229c2641b1a7d16520b3743802
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-09-27 15:16:34 -0300

    Estilos gerais

--- src/App.vue
+++ src/App.vue
@@ -26,11 +26,9 @@ export default {
   flex-direction: column;
   min-height: 100vh;
 }
-
 #main {
   flex: 1;
 }
-
 body,
 ul,
 li,
@@ -40,27 +38,22 @@ p {
   padding: 0;
   margin: 0;
 }
-
 ul {
   list-style: none;
 }
-
 body {
   font-family: 'Avenir', Arial, Helvetica, sans-serif;
   color: #345;
   background: url('./assets/pattern.svg') repeat top;
 }
-
 a {
   color: #345;
   text-decoration: none;
 }
-
 img {
   width: 100%;
   display: block;
 }
-
 .btn {
   display: block;
   padding: 10px 30px;
@@ -75,9 +68,27 @@ img {
   font-family: 'Avenir', Arial, Helvetica, sans-serif;
   cursor: pointer;
 }
-
 .btn:hover {
   background-color: #65d;
   transform: scale(1.1);
+}
+input,
+textarea {
+  border-radius: 8px;
+  border: 1px solid #fff;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  transition: all 0.3s;
+  font-size: 1rem;
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
+  margin-bottom: 15px;
+}
+input:hover,
+input:focus,
+textarea:hover,
+textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  border-color: #87f;
 }
 </style>
