1cdd816a3ae954548798740d28875f3fbc0d4c94
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-09-27 14:20:38 -0300

    Estrutura da busca de produtos

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>BUSCAR</div>
+  <form>
+    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
 </template>
 
 <script>
 export default {
   name: 'ProdutosBuscar',
+  data() {
+    return {
+      busca: '',
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
 };
 </script>
 
