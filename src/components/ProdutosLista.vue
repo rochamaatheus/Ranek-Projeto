6c072486ab79e997ba41c51212a55c910770dec4
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-09-27 14:52:15 -0300

    v-if em produtos

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,14 +1,21 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos[0]"
-        :src="produto.fotos[0].src"
-        alt="produto.fotos[0].titulo"
-      />
-      <p class="preco">{{ produto.preco }}</p>
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p>{{ produto.descricao }}</p>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div v-for="produto in produtos" :key="produto.id" class="produto">
+        <router-link to="/">
+          <img
+            v-if="produto.fotos[0]"
+            :src="produto.fotos[0].src"
+            alt="produto.fotos[0].titulo"
+          />
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p>Busca sem resultados.</p>
     </div>
   </section>
 </template>
