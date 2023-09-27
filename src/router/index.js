30d875920c47cdaec32d143942f2813fb5942065
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-09-27 11:01:02 -0300

    Definindo a Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '@/views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -7,6 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    component: Home,
   },
 ];
 
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Compre ou Venda</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TheHome',
+};
+</script>
+
+<style></style>
