6cb6922d55ed21775bcfea0d07d11c8d789c2112
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-09-27 11:02:16 -0300

    Importando os componentes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,15 @@
 </template>
 
 <script>
+import ProdutosBuscar from '@/components/ProdutosBuscar.vue';
+import ProdutosLista from '@/components/ProdutosLista.vue';
+
 export default {
   name: 'TheHome',
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
 };
 </script>
 
