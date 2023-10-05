7b84d30a0d3ddb3fdf0ebd1d6264b9df27d76dc8
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-05 15:03:43 -0300

    Função para preencher CEP

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -26,6 +26,7 @@
 
 <script>
 import { mapFields } from '@/helpers.js';
+import { getCep } from '@/services.js';
 
 export default {
   name: 'UsuarioForm',
@@ -45,6 +46,16 @@ export default {
       base: 'usuario',
       mutation: 'UPDATE_USUARIO',
     }),
+    preencherCep() {
+      const cep = this.cep.replace(/\D/g, '');
+      if (cep.length == 8)
+        getCep(cep).then((r) => {
+          this.rua = r.data.logradouro;
+          this.bairro = r.data.bairro;
+          this.estado = r.data.uf;
+          this.cidade = r.data.localidade;
+        });
+    },
   },
 };
 </script>
