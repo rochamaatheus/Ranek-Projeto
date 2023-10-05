3deed4c346027e252c002e535b8afde5e107bfee
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-05 15:10:04 -0300

    Computed => Methods

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -46,6 +46,8 @@ export default {
       base: 'usuario',
       mutation: 'UPDATE_USUARIO',
     }),
+  },
+  methods: {
     preencherCep() {
       const cep = this.cep.replace(/\D/g, '');
       if (cep.length == 8)
