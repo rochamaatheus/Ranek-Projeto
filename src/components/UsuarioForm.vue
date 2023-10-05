00179ec296c1058ddd682f034154e8c5a04e7f56
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-05 11:26:54 -0300

    Estrutura inicial

---
+++ src/components/UsuarioForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <label for="name">Nome</label>
+    <input type="text" name="name" id="name" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'UsuarioForm',
+};
+</script>
+
+<style scoped></style>
