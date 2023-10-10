5d25b684d2d5d92758b13fabb49b2d72e7500f98
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-10 14:52:19 -0300

    Organizando router-links

--- src/views/usuario/Usuario.vue
+++ src/views/usuario/Usuario.vue
@@ -2,11 +2,15 @@
   <section class="usuario">
     <nav class="sidenav">
       <ul>
-        <li><router-link to="/">Produtos</router-link></li>
-        <li><router-link to="/">Compras</router-link></li>
-        <li><router-link to="/">Vendas</router-link></li>
-        <li><router-link to="/">Editar Usuário</router-link></li>
-        <li><router-link to="/">Deslogar</router-link></li>
+        <li><router-link :to="{ name: 'produtos' }">Produtos</router-link></li>
+        <li><router-link :to="{ name: 'compras' }">Compras</router-link></li>
+        <li><router-link :to="{ name: 'vendas' }">Vendas</router-link></li>
+        <li>
+          <router-link :to="{ name: 'usuario-editar' }"
+            >Editar Usuário</router-link
+          >
+        </li>
+        <li><button @click.prevent="deslogar">Deslogar</button></li>
       </ul>
     </nav>
     <transition mode="out-in">
