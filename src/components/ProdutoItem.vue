8e840dbd0edc8894233eae0ddc4d88a55a3e65cd
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-10 17:42:01 -0300

    v-if

--- src/components/ProdutoItem.vue
+++ src/components/ProdutoItem.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="produto" v-if="produto">
-    <router-link :to="{ name: 'produto', params: { id: produto.id } }"
-      >Ver Produto</router-link
-    >
+    <router-link :to="{ name: 'produto', params: { id: produto.id } }">
+      <img
+        v-if="produto.fotos[0]"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p>Ver Produto</p>
+    </router-link>
     <div class="info">
       <p class="preco">{{ produto.preco | numeroPreco }}</p>
       <h2 class="titulo">{{ produto.nome }}</h2>
