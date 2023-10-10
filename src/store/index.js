9dc3b699d901fab37b0a49fba419b35d83095005
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-10 16:56:46 -0300

    Mutations e Actions para modificar a lista de produtos

--- src/store/index.js
+++ src/store/index.js
@@ -20,6 +20,7 @@ export default new Vuex.Store({
       cidade: '',
       estado: '',
     },
+    usuario_produtos: null,
   },
   getters: {},
   mutations: {
@@ -29,8 +30,19 @@ export default new Vuex.Store({
     UPDATE_USUARIO(state, payload) {
       state.usuario = Object.assign(state.usuario, payload);
     },
+    UPDATE_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos = Object.assign(state.usuario_produtos, payload);
+    },
+    ADD_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos.unshift(payload);
+    },
   },
   actions: {
+    getUsuarioProdutos(context, payload) {
+      api.get(`/produto?usuario_id=${context.state.usuario.id}`).then((r) => {
+        context.commit('UPDATE_USUARIO_PRODUTOS', r.data);
+      });
+    },
     getUsuario(context, payload) {
       return api.get(`/usuario/${payload}`).then((r) => {
         context.commit('UPDATE_USUARIO', r.data);
