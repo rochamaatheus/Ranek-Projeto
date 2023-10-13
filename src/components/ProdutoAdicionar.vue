7ae590ba6fbc030874c71b5996f86a31f5dafb68
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-13 15:43:45 -0300

    Estrutura inicial

--- src/components/ProdutoAdicionar.vue
+++ src/components/ProdutoAdicionar.vue
@@ -1,12 +1,39 @@
 <template>
-  <section>
-    <p>Produto Adicionar</p>
-  </section>
+  <form class="adicionar-produto">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" name="nome" v-model="produto.nome" />
+    <label for="preco">Preço (USD)</label>
+    <input type="number" id="preco" name="preco" v-model="produto.preco" />
+    <label for="fotos">Fotos</label>
+    <input type="file" id="fotos" name="fotos" ref="fotos" />
+    <label for="descricao">Descrição</label>
+    <textarea
+      name="descricao"
+      id="descricao"
+      v-model="produto.descricao"
+    ></textarea>
+    <input
+      class="btn"
+      type="button"
+      value="Adicionar Produto"
+      @click.prevent="adicionarProduto"
+    />
+  </form>
 </template>
 
 <script>
 export default {
   name: 'ProdutoAdicionar',
+  data() {
+    return {
+      produto: {
+        nome: '',
+        preco: '',
+        fotos: '',
+        descricao: '',
+      },
+    };
+  },
 };
 </script>
 
