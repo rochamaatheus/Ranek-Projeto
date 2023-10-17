a2c3a8b4a47dc5f5266170e732b8afc534f6c543
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-17 10:04:26 -0300

    Estrutura Inicial

--- src/views/usuario/UsuarioEditar.vue
+++ src/views/usuario/UsuarioEditar.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>Usuario Editar</div>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">
+        Atualizar Usuário
+      </button>
+    </UsuarioForm>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from '@/components/UsuarioForm.vue';
+
 export default {
-  name: 'UsuarioProdutos',
+  name: 'UsuarioEditar',
+  components: {
+    UsuarioForm,
+  },
 };
 </script>
 
