ddab1be2773b57145d0bb3f126ba4536dbfb55b0
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-17 11:17:07 -0300

    Lógica inicial FinalizarCompra

--- src/components/FinalizarCompra.vue
+++ src/components/FinalizarCompra.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    <p>Finalizar compra</p>
-  </div>
+  <section>
+    <h2>Endereço de Envio</h2>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarCompra">
+        Finalizar Compra
+      </button>
+    </UsuarioForm>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from './UsuarioForm.vue';
+
 export default {
   name: 'FinalizarCompra',
+  components: { UsuarioForm },
+  props: ['produto'],
+  methods: {
+    atualizarCompra() {
+      alert('Comprou');
+    },
+  },
 };
 </script>
 
