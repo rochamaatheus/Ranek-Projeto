7a3bc63f8046165639acd5ebf339628c24e552b2
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-17 10:09:12 -0300

    Função básica para atualizar o usuário

--- src/views/usuario/UsuarioEditar.vue
+++ src/views/usuario/UsuarioEditar.vue
@@ -10,11 +10,26 @@
 
 <script>
 import UsuarioForm from '@/components/UsuarioForm.vue';
+import { api } from '@/services.js';
 
 export default {
   name: 'UsuarioEditar',
   components: {
     UsuarioForm,
+  },
+  methods: {
+    atualizarUsuario() {
+      api
+        .put(
+          `/usuario/${this.$store.state.usuario.id}`,
+          this.$store.state.usuario
+        )
+        .then(() => {
+          this.$store.dispatch('getUsuario');
+          this.$router.push({ name: 'usuario' });
+        })
+        .catch((err) => console.log(err.response));
+    },
   },
 };
 </script>
