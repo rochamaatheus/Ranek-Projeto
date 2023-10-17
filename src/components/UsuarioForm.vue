c028cf0748dec50fa5d85313db0867fd87af932f
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2023-10-17 11:20:18 -0300

    Lógica mostrar dados de login

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -1,11 +1,13 @@
 <template>
   <form>
-    <label for="nome">Nome</label>
-    <input id="nome" name="nome" type="text" v-model="nome" />
-    <label for="email">Email</label>
-    <input id="email" name="email" type="email" v-model="email" />
-    <label for="senha">Senha</label>
-    <input id="senha" name="senha" type="password" v-model="senha" />
+    <div class="usuario" v-if="mostrarDadosLogin">
+      <label for="nome">Nome</label>
+      <input id="nome" name="nome" type="text" v-model="nome" />
+      <label for="email">Email</label>
+      <input id="email" name="email" type="email" v-model="email" />
+      <label for="senha">Senha</label>
+      <input id="senha" name="senha" type="password" v-model="senha" />
+    </div>
     <label for="cep">Cep</label>
     <input
       id="cep"
@@ -51,6 +53,9 @@ export default {
       base: 'usuario',
       mutation: 'UPDATE_USUARIO',
     }),
+    mostrarDadosLogin() {
+      return !this.$store.state.login || this.$route.name === 'usuario-editar';
+    },
   },
   methods: {
     preencherCep() {
